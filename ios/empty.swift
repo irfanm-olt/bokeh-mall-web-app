c0183bdb445f178806ac62d779824dd7475f0653
Muhammed Irfan <muhammed.irfan@optiologic.co.in>
2022-01-17 23:11:54 +0530

    repo changed

--- ios/empty.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Empty.swift
-//  rn_oreo
-//
-//  Created by Dang Ngoc on 9/30/20.
-//
-
-import Foundation
--- src/containers/vendor/index.js
+++
@@ -1,6 +0,0 @@
-import ItemVendorLoading from './Loading';
-import ItemVendorWcfm from './wcfm/ItemVendor';
-
-const ItemVendor = ItemVendorWcfm;
-
-export {ItemVendor, ItemVendorLoading};
